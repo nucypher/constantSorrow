efd48160c0c4ee6517adc22aa64a2f2dd014e7d5
Kieran Prasch <kieranprasch@gmail.com>
2018-10-07 13:12:55 -0700

    Packaging and versioning for alpha publication in the cheese shop.

---
+++ constant_sorrow/__about__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__", "__email__", "__license__", "__copyright__", "__url__"
+]
+
+__title__ = "constant_sorrow"
+
+__url__ = "https://github.com/nucypher/constantSorrow"
+
+__summary__ = "I am the man of constant sorrow; I've seen special values as ints, all my days."
+
+__version__ = "0.1.0-alpha.0"
+
+__author__ = "NuCypher"
+
+__license__ = "Apache Software License v2.0"
+
+__email__ = "dev@nucypher.com"
+
+__copyright__ = 'Copyright (C) 2018 NuCypher'
--- constant_sorrow/__init__.py
+++ constant_sorrow/__init__.py
@@ -1,3 +1,7 @@
+from constant_sorrow.__about__ import __author__, __summary__, __title__, __version__
+__all__ = ["__title__", "__summary__", "__version__", "__author__", ]
+
+
 from bytestring_splitter import BytestringSplitter
 _digest_length = 8
 
